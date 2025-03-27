bc622af72c7ae677e780b8bb299d47742171df88
Caio Mega <caio.mega@aluno.senai.br>
2025-03-27 08:17:08 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'GETUP BR - Eleve sua marca',
+  description: 'Conquiste o reconhecimento da sua marca conosco',
+  generator: 'caio',
 }
 
 export default function RootLayout({
